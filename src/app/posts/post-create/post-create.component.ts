f22f55c3312c63346a4f62b6c69573859b4c5c2d
Alves Schulze <laschulze@indracompany.com>
2019-10-29 11:12:25 -0300

    initial commit

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-post-create',
+    templateUrl: './post-create.component.html'
+})
+export class PostCreateComponent{
+    enteredValue = "new value";
+    newPost = 'NO CONTENT';
+
+    onAddPost(postInput: HTMLTextAreaElement){
+        this.newPost = this.enteredValue
+    }
+}
