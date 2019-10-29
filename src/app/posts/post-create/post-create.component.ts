747784d5ae5d60c25ec8ae9cd87428882cd05708
Alves Schulze <laschulze@indracompany.com>
2019-10-29 15:15:47 -0300

    utilizando o ngModel em forms

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,21 +1,25 @@
 import { Component, EventEmitter, Output } from "@angular/core";
 import { Post } from "../post.model";
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-post-create',
     templateUrl: './post-create.component.html',
     styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent{
+export class PostCreateComponent {
     enteredTitle = "";
     enteredContent = "";
     @Output()
     postCreated = new EventEmitter<Post>();
-    
-    onAddPost(){
+
+    onAddPost(form: NgForm) {
+        if(form.invalid){
+            return;
+        }
         const post: Post = {
-            title: this.enteredTitle,
-            content: this.enteredContent
+            title: form.value.title,
+            content: form.value.content
         };
         this.postCreated.emit(post);
     }
